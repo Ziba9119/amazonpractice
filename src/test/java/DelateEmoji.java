c611ca7ea862521cc566f6665eebfe57e6bbebd3
Ziba9119 <ziba.ablet@gmail.com>
2022-01-19 20:19:39 -0800

    add DeleteEmoji.java

---
+++ src/test/java/DelateEmoji.java
@@ -0,0 +1,8 @@
+public class DelateEmoji {
+    public static void main(String[] args) {
+        System.out.println("code");
+
+
+        //not finished
+    }
+}
--- src/test/java/Uploadfile.java
+++ src/test/java/Uploadfile.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Uploadfile {
+public class Uploadfile {
+    public static void main(String[] args) {
+        System.out.println("upload file");
+
+
+
+
+        //this is ziba's upload file class
+    }
 }
